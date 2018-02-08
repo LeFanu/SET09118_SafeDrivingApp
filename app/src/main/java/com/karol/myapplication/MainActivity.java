6314145b20c7fddb933fbd7223549a8bb783a861
LeFanu <pasierbkarol@gmail.com>
2018-02-08 11:51:21 +0000

    UI built from wireframes with use of styles, colours and strings

--- app/src/main/java/com/karol/myapplication/MainActivity.java
+++ app/src/main/java/com/karol/myapplication/MainActivity.java
@@ -30,38 +30,21 @@ public class MainActivity extends AppCompatActivity {
         drivingStatusIndicator.setTextColor(getResources().getColor(R.color.driving_ok));
 
 
-        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        List<Sensor> deviceSensors = sensorManager.getSensorList(Sensor.TYPE_ALL);
-
-
-        if (sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT) != null)
-        {
-            lightSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
-        }
-        else
-        {
-            //Snackbar.make(view, "Sorry no light lightSensor", Snackbar.LENGTH_LONG).setAction("Action", null).show();
-        }
-        lightSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
-        sensorManager.registerListener(mSensorEventListener, lightSensor, SensorManager.SENSOR_DELAY_NORMAL);
+//        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+//        List<Sensor> deviceSensors = sensorManager.getSensorList(Sensor.TYPE_ALL);
+//
+//
+//        if (sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT) != null)
+//        {
+//            lightSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+//        }
+//        else
+//        {
+//            //Snackbar.make(view, "Sorry no light lightSensor", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+//        }
+//        lightSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+//        sensorManager.registerListener(mSensorEventListener, lightSensor, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
-    TextView textbox ;
-    private SensorEventListener mSensorEventListener = new SensorEventListener() {
-
-        @Override
-        public final void onSensorChanged(SensorEvent event) {
-            float lux = event.values[0];
-            textbox = (TextView) findViewById(R.id.lightText);
-            // Do something with this sensor value
-            //Toast.makeText(getApplicationContext(), "light is  " + lux, Toast.LENGTH_SHORT).show();
-            textbox.setText("Light Sensor value is: " + lux);
-        }
-
-        @Override
-        public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
-        }
-    };
 
 }

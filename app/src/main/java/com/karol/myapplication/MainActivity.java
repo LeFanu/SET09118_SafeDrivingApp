6334a461f06bfae947d5cde5daaa87ed17ae80aa
Alexis Canevali <alexis.canevali@gmail.com>
2018-02-08 11:58:32 +0000

    Location attempt
    (Not successful for now)

--- app/src/main/java/com/karol/myapplication/MainActivity.java
+++ app/src/main/java/com/karol/myapplication/MainActivity.java
@@ -5,20 +5,34 @@ import android.content.res.ColorStateList;
 import android.content.res.Resources;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.hardware.SensorManager;
 import android.hardware.Sensor;
+import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 
-import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
 
     private SensorManager sensorManager;
     private Sensor lightSensor;
+    private LocationManager locationManager;
+    private Location previousLocation = null;
+    private float distanceBetweenNewAndOldLocation;
+    private TextView speedTextbox;
+    private FusedLocationProviderClient mFusedLocationClient;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,26 +43,112 @@ public class MainActivity extends AppCompatActivity {
         drivingStatusIndicator.setText("OK");
         drivingStatusIndicator.setTextColor(getResources().getColor(R.color.driving_ok));
 
+        speedTextbox = (TextView) findViewById(R.id.speedText);
 
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        List<Sensor> deviceSensors = sensorManager.getSensorList(Sensor.TYPE_ALL);
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+       /* if (!isGooglePlayServicesAvailable()) {
+            finish();
+        }
+        createLocationRequest();
+
+        GoogleApiClient googleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();*/
+
+        //List<Sensor> deviceSensors = sensorManager.getSensorList(Sensor.TYPE_ALL);
 
 
         if (sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT) != null)
         {
             lightSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+            sensorManager.registerListener(mSensorEventListener, lightSensor, SensorManager.SENSOR_DELAY_NORMAL);
         }
         else
         {
-            //Snackbar.make(view, "Sorry no light lightSensor", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+            Toast.makeText(this.getApplicationContext(),"No light sensor on this device",Toast.LENGTH_SHORT);
         }
-        lightSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
-        sensorManager.registerListener(mSensorEventListener, lightSensor, SensorManager.SENSOR_DELAY_NORMAL);
+
+}
+
+mFusedLocationClient.getLastLocation()
+        .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+        @Override
+        public void onSuccess(Location location) {
+            // Got last known location. In some rare situations this can be null.
+            if (location != null) {
+                // Logic to handle location object
+            }
+        }
+    });
+
+  /*  @Override
+    public void onConnected(Bundle bundle) {
+        Log.d(TAG, "onConnected - isConnected ...............: " + mGoogleApiClient.isConnected());
+        startLocationUpdates();
     }
+
+    protected void startLocationUpdates() {
+        PendingResult<Status> pendingResult = LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+        Log.d(TAG, "Location update started ..............: ");
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.d(TAG, "Connection failed: " + connectionResult.toString());
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        if(previousLocation!=null){
+            //distanceBetweenNewAndOldLocation = location.distanceTo(previousLocation);
+            distanceBetweenNewAndOldLocation = location.getSpeed();
+            speedTextbox.setText("Speed :" + distanceBetweenNewAndOldLocation + "m/s");
+            Toast.makeText(getApplicationContext(),"Location changed",Toast.LENGTH_SHORT);
+
+        }
+    }*/
+
+    /*@Override
+    public void onLocationChanged(Location location) {
+        Log.d(TAG, "Firing onLocationChanged..............................................");
+        mCurrentLocation = location;
+        mLastUpdateTime = DateFormat.getTimeInstance().format(new Date());
+
+    }*/
+
+   /* @Override
+    protected void onPause() {
+        super.onPause();
+        stopLocationUpdates();
+    }
+
+    protected void stopLocationUpdates() {
+        LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, (com.google.android.gms.location.LocationListener) this);
+        Log.d(TAG, "Location update stopped .......................");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (mGoogleApiClient.isConnected()) {
+            startLocationUpdates();
+            Log.d(TAG, "Location update resumed .....................");
+        }
+    }*/
+
 
     TextView textbox ;
     private SensorEventListener mSensorEventListener = new SensorEventListener() {
-
         @Override
         public final void onSensorChanged(SensorEvent event) {
             float lux = event.values[0];
@@ -64,4 +164,36 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
-}
+    /*private boolean isGooglePlayServicesAvailable() {
+        int status = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+        if (ConnectionResult.SUCCESS == status) {
+            return true;
+        } else {
+            GooglePlayServicesUtil.getErrorDialog(status, this, 0).show();
+            return false;
+        }
+    }*/
+
+   /* mFusedLocationClient.getLastLocation()
+            .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+        @Override
+        public void onSuccess(Location location) {
+            // Got last known location. In some rare situations this can be null.
+            if (location != null) {
+                // Logic to handle location object
+            }
+        }
+    });*/
+
+   /* protected void createLocationRequest() {
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(INTERVAL);
+        mLocationRequest.setFastestInterval(FASTEST_INTERVAL);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }*/
+
+
+    //LocationListener locationListener = new LocationListener() {
+
+};
+

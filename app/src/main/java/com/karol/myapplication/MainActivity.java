9c0d4c83dd368f1fbe8816b19e598e6ffe20d4ee
Alexis Canevali <alexis.canevali@gmail.com>
2018-02-11 16:18:48 +0000

    Merging fixed

--- app/src/main/java/com/karol/myapplication/MainActivity.java
+++ app/src/main/java/com/karol/myapplication/MainActivity.java
@@ -1,38 +1,30 @@
 package com.karol.myapplication;
 
 import android.Manifest;
-<<<<<<< HEAD
+
+import android.annotation.SuppressLint;
 import android.content.Context;
-=======
-import android.app.IntentService;
 import android.app.PendingIntent;
-import android.content.Context;
 import android.content.Intent;
->>>>>>> fc775aaaf96215324a1014ecefd8c6e4790f1726
 import android.content.pm.PackageManager;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.hardware.Sensor;
 import android.location.Location;
-<<<<<<< HEAD
-=======
+
 import android.location.LocationManager;
->>>>>>> fc775aaaf96215324a1014ecefd8c6e4790f1726
+import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-<<<<<<< HEAD
 import android.hardware.SensorManager;
 import android.hardware.Sensor;
-=======
 import android.util.Log;
->>>>>>> fc775aaaf96215324a1014ecefd8c6e4790f1726
 import android.widget.TextView;
-
-<<<<<<< HEAD
+import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderApi;
 import com.google.android.gms.location.LocationServices;
@@ -47,7 +39,6 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
-=======
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.ActivityRecognition;
@@ -57,28 +48,18 @@ import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 
->>>>>>> fc775aaaf96215324a1014ecefd8c6e4790f1726
-
-public class MainActivity extends AppCompatActivity implements SensorEventListener, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
-
-<<<<<<< HEAD
 public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, com.google.android.gms.location.LocationListener {
-=======
     //fields used in GUI
-    private TextView speedTextbox;
     private TextView drivingTimeHours;
     private TextView drivingTimeMinutes;
     String TAG = "Karol";
->>>>>>> fc775aaaf96215324a1014ecefd8c6e4790f1726
 
     //Sensors properties
     private SensorManager sensorManager;
-<<<<<<< HEAD
     private Sensor lightSensor;
 
     private TextView speedTextbox;
 
-    private Location previousLocation = null;
     private Location currentLocation = null;
     private String mCurrentLocationUpdateTime = null;
     private String mpreviousLocationUpdateTime = null;
@@ -87,10 +68,8 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
     private Date dateCurrentLocation;
     long differenceTime = 0;
 
-    private GoogleApiClient googleApiClient;
     private LocationRequest locationRequest;
     private FusedLocationProviderApi locationProviderApi = LocationServices.FusedLocationApi;
-=======
     private LocationManager locationManager;
     private Location previousLocation = null;
     private float distanceBetweenNewAndOldLocation;
@@ -103,7 +82,6 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
     private GoogleApiClient activityRecognitionGoogleApiClient;
 
     private ActivityRecognitionIntentService activityDetection = new ActivityRecognitionIntentService("Is It Driving");
->>>>>>> fc775aaaf96215324a1014ecefd8c6e4790f1726
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -111,18 +89,20 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
 
         setContentView(R.layout.activity_main);
 
-<<<<<<< HEAD
         /*Light sensor*/
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        lightSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
-        sensorManager.registerListener(mSensorEventListener, lightSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.CUPCAKE) {
+            lightSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.CUPCAKE) {
+            sensorManager.registerListener(mSensorEventListener, lightSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        }
 
         /*GPS*/
 
         sdf = new SimpleDateFormat("dd-M-yyyy hh:mm:ss");
 
         TextView drivingStatusIndicator = (TextView) findViewById(R.id.driving_status_indicator);
-=======
         //@Karol: accessing Driving Status textview. Required only when on the move
         /*TextView drivingStatusIndicator = (TextView) findViewById(R.id.driving_status_indicator);
 >>>>>>> fc775aaaf96215324a1014ecefd8c6e4790f1726
@@ -130,60 +110,24 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
         drivingStatusIndicator.setTextColor(getResources().getColor(R.color.driving_ok));*/
 
         speedTextbox = (TextView) findViewById(R.id.speed_value);
-
-<<<<<<< HEAD
-        speedTextbox = (TextView) findViewById(R.id.speed_value);
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
 
-=======
-
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        accelerometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER); //using accelerometer
-        registerAccelerometerListener();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.CUPCAKE) {
+            accelerometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER); //using accelerometer
+        }
+        //registerAccelerometerListener();
 
 
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
-        //@Karol: Apparently needed for getLastLocation()
-        //Unfortunately it has a return so leaves the method once reaches that point
-       /* if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            Log.d(TAG, "Permissions granted");
-            return;
-        }*/
-/*        mFusedLocationClient.getLastLocation()
-                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
-                    @Override
-                    public void onSuccess(Location location) {
-                        // Got last known location. In some rare situations this can be null.
-                        if (location != null) {
-                            // Logic to handle location object
-                            float speed = location.getSpeed();
-                            Log.d(TAG,"Speed is " + speed);
-                        }
-                    }
-                });*/
-
-        /*if (!isGooglePlayServicesAvailable()) {
-            finish();
-        }
-        createLocationRequest();
-*/
->>>>>>> fc775aaaf96215324a1014ecefd8c6e4790f1726
         googleApiClient = new GoogleApiClient.Builder(this)
                 .addApi(LocationServices.API)
                 .addApi(ActivityRecognition.API)
                 .addConnectionCallbacks(this)
                 .addOnConnectionFailedListener(this)
                 .build();
-<<<<<<< HEAD
 
         locationRequest = new LocationRequest();
         locationRequest.setInterval(3000);
@@ -202,7 +146,10 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
     private SensorEventListener mSensorEventListener = new SensorEventListener() {
         @Override
         public final void onSensorChanged(SensorEvent event) {
-            float lux = event.values[0];
+            float lux = 0;
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.CUPCAKE) {
+                lux = event.values[0];
+            }
             textbox = (TextView) findViewById(R.id.visibility_value);
             // Do something with this sensor value
             //Toast.makeText(getApplicationContext(), "light is  " + lux, Toast.LENGTH_SHORT).show();
@@ -222,10 +169,6 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
     /*
     * Speed calculator code
     * */
-    @Override
-    public void onConnected(@Nullable Bundle bundle) {
-        requestLocationUpdates();
-    }
 
     private void requestLocationUpdates() {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
@@ -239,7 +182,6 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
             return;
         }
         LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient, locationRequest, this);
-=======
 
         googleApiClient.connect();
 
@@ -254,12 +196,12 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
 
 
     //@Karol - Extracted this method to avoid copying lines
-    private void registerAccelerometerListener()
+    /*private void registerAccelerometerListener()
     {
         sensorManager.registerListener(this, accelerometerSensor, SensorManager.SENSOR_DELAY_NORMAL);
 
-    }
-
+    }*/
+    /*
     @Override
     public void onSensorChanged(SensorEvent event) {
 
@@ -280,108 +222,11 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-
-        //stopLocationUpdates();
-
-        registerAccelerometerListener();
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        /*if (mGoogleApiClient.isConnected()) {
-            startLocationUpdates();
-            Log.d(TAG, "Location update resumed .....................");
-        }*/
-
-
-    }
+    }*/
 
     @Override
     public void onConnected(@Nullable Bundle bundle) {
-        //Log.d("Karol", "onConnected - isConnected ...............: " + mGoogleApiClient.isConnected());
-        //startLocationUpdates();
-        Intent intent = new Intent( this, MainActivity.class );
-        PendingIntent pendingIntent = PendingIntent.getService( this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT );
-        ActivityRecognition.ActivityRecognitionApi.requestActivityUpdates( googleApiClient, 3000, pendingIntent );
-
->>>>>>> fc775aaaf96215324a1014ecefd8c6e4790f1726
-    }
-
-    @Override
-    public void onConnectionSuspended(int i) {
-
-    }
-
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-<<<<<<< HEAD
-        Toast.makeText(this,connectionResult.getErrorMessage(),Toast.LENGTH_LONG).show();
-=======
-        //TAG is the name you use to filter the log results. It can be anything
-        Log.d(TAG, "Connection failed: " + connectionResult.toString());
->>>>>>> fc775aaaf96215324a1014ecefd8c6e4790f1726
-    }
-
-
-
-
-  /*
-    protected void startLocationUpdates() {
-        PendingResult<Status> pendingResult = LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
-        Log.d(TAG, "Location update started ..............: ");
-    }
-
-
-    @Override
-    public void onLocationChanged(Location location) {
-        previousLocation = currentLocation;
-        mpreviousLocationUpdateTime = mCurrentLocationUpdateTime;
-        currentLocation = location;
-        mCurrentLocationUpdateTime = new SimpleDateFormat("dd-M-yyyy hh:mm:ss").format(new Date(System.currentTimeMillis()));
-
-        if((mpreviousLocationUpdateTime!=null)&&(mCurrentLocationUpdateTime!=null)){
-            try {
-                datePreviousLocation = sdf.parse(mpreviousLocationUpdateTime);
-            } catch (ParseException e) {
-                e.printStackTrace();
-            }
-
-<<<<<<< HEAD
-            try {
-                dateCurrentLocation = sdf.parse(mCurrentLocationUpdateTime);
-            } catch (ParseException e) {
-                e.printStackTrace();
-            }
-
-            differenceTime = dateCurrentLocation.getTime() - datePreviousLocation.getTime();
-            differenceTime = differenceTime/1000;//millisec to seconds
-           // Toast.makeText(this, String.valueOf(differenceTime), Toast.LENGTH_SHORT).show();//Should display value arround 3
-
-        }else{
-            differenceTime = 0;
-        }
-
-        if((previousLocation==null)||(currentLocation==null)){
-            speedTextbox.setText("Calibrating...");
-        } else {
-            double distance = currentLocation.distanceTo(previousLocation);
-
-            double speed;
-            if(differenceTime!=0){
-                speed= Math.round((distance/differenceTime)*100.0)/100.0;
-            }else{
-                speed= 0.0;
-            }
-            speedTextbox.setText(Double.toString(speed));
-
-        }
-        //Toast.makeText(this, "Speed updated", Toast.LENGTH_SHORT).show();
+        requestLocationUpdates();
     }
 
     @Override
@@ -403,54 +248,80 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
         super.onPause();
         LocationServices.FusedLocationApi.removeLocationUpdates(googleApiClient,this);
     }
-=======
-Log.d(TAG, "Firing onLocationChanged..............................................");
-        mCurrentLocation = location;
-        mLastUpdateTime = DateFormat.getTimeInstance().format(new Date());
-        }
-    }*/
-
-
-/*
-    protected void stopLocationUpdates() {
-        LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, (com.google.android.gms.location.LocationListener) this);
-        Log.d(TAG, "Location update stopped .......................");
-    }
-*/
-
-    /*private boolean isGooglePlayServicesAvailable() {
-        int status = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
-        if (ConnectionResult.SUCCESS == status) {
-            return true;
-        } else {
-            GooglePlayServicesUtil.getErrorDialog(status, this, 0).show();
-            return false;
-        }
-    }*/
-
-   /* mFusedLocationClient.getLastLocation()
-            .addOnSuccessListener(this, new OnSuccessListener<Location>() {
-        @Override
-        public void onSuccess(Location location) {
-            // Got last known location. In some rare situations this can be null.
-            if (location != null) {
-                // Logic to handle location object
-            }
-        }
-    });*/
-
-   /* protected void createLocationRequest() {
-        mLocationRequest = new LocationRequest();
-        mLocationRequest.setInterval(INTERVAL);
-        mLocationRequest.setFastestInterval(FASTEST_INTERVAL);
-        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-    }*/
->>>>>>> fc775aaaf96215324a1014ecefd8c6e4790f1726
 
     @Override
     protected void onStop() {
         super.onStop();
         googleApiClient.disconnect();
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+        Toast.makeText(this,connectionResult.getErrorMessage(),Toast.LENGTH_LONG).show();
+        //TAG is the name you use to filter the log results. It can be anything
+        Log.d(TAG, "Connection failed: " + connectionResult.toString());
+    }
+
+   /* @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        //Log.d("Karol", "onConnected - isConnected ...............: " + mGoogleApiClient.isConnected());
+        //startLocationUpdates();
+        Intent intent = new Intent( this, MainActivity.class );
+        PendingIntent pendingIntent = PendingIntent.getService( this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT );
+        ActivityRecognition.ActivityRecognitionApi.requestActivityUpdates( googleApiClient, 3000, pendingIntent );
+
+>>>>>>> fc775aaaf96215324a1014ecefd8c6e4790f1726
+    }*/
+
+    @Override
+    public void onLocationChanged(Location location) {
+        previousLocation = currentLocation;
+        mpreviousLocationUpdateTime = mCurrentLocationUpdateTime;
+        currentLocation = location;
+        mCurrentLocationUpdateTime = new SimpleDateFormat("dd-M-yyyy hh:mm:ss").format(new Date(System.currentTimeMillis()));
+
+        if((mpreviousLocationUpdateTime!=null)&&(mCurrentLocationUpdateTime!=null)){
+            try {
+                datePreviousLocation = sdf.parse(mpreviousLocationUpdateTime);
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+
+            try {
+                dateCurrentLocation = sdf.parse(mCurrentLocationUpdateTime);
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+
+            differenceTime = dateCurrentLocation.getTime() - datePreviousLocation.getTime();
+            differenceTime = differenceTime/1000;//millisec to seconds
+            // Toast.makeText(this, String.valueOf(differenceTime), Toast.LENGTH_SHORT).show();//Should display value arround 3
+
+        }else{
+            differenceTime = 0;
+        }
+
+        if((previousLocation==null)||(currentLocation==null)){
+            speedTextbox.setText("Calibrating...");
+        } else {
+            double distance = currentLocation.distanceTo(previousLocation);
+
+            double speed;
+            if(differenceTime!=0){
+                speed= Math.round((distance/differenceTime)*100.0)/100.0;
+            }else{
+                speed= 0.0;
+            }
+            speedTextbox.setText(Double.toString(speed));
+            //Toast.makeText(this, Double.toString(speed), Toast.LENGTH_SHORT).show();
+        }
+        //Toast.makeText(this, "Speed updated", Toast.LENGTH_SHORT).show();
     }
 
     /*
